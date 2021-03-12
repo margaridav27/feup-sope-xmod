95abf6e44872fd2f5cecee8a1e63ee1feb5f3dd4
Margarida Vieira <margaridajcv.27@gmail.com>
2021-03-12 10:16:45 +0000

    set logging instant precision

--- project/src/log.c
+++ project/src/log.c
@@ -42,7 +42,7 @@ int logEvent(int pid, event_t event, char *info) {
         default: break;
     }
 
-    fprintf(logFP, "%f ; %d ; %s ; %s\n", getElapsed(), pid, action, info);
+    fprintf(logFP, "%.3f ; %d ; %s ; %s\n", getElapsed(), pid, action, info);
 
     return 0;
 }
