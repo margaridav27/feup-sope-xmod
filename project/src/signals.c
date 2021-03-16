050c3b6072911c9006e400876f6d01ad4cac2563
Tomás Vicente <up201904609@fe.up.pt>
2021-03-16 15:26:46 +0000

    Signal processing.

--- project/src/signals.c
+++ project/src/signals.c
@@ -2,6 +2,9 @@
 #include "../include/utils.h"
 #include <signal.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <sys/wait.h>
 
 // Parent flags
 static volatile sig_atomic_t prompt = false; // Whether the user should be prompted
@@ -31,4 +34,63 @@ int setUpSignals(void) {
         signal(SIGTERM, child_sigterm_handler);
     }
     return 0;
+}
+
+void terminateProgramParent(void) {
+    //COMBACK: LOG
+    killpg(0, SIGCONT);
+    killpg(0, SIGTERM);
+    leave(1);
+}
+
+void continueProgramParent(void) {
+    //COMBACK: LOG
+    killpg(0, SIGCONT);
+}
+
+bool checkParentAction(void) {
+    //COMBACK: LOG
+    if (!prompt) return false;
+    unsigned char answer;
+    do {
+        puts("Are you sure that you want to terminate? (Y/N)");
+        answer = toupper(getchar());
+        getchar(); // Discard newline
+    } while (answer != 'Y' && answer != 'N');
+    prompt = false;
+    if (answer == 'N') {
+        continueProgramParent();
+        return false;
+    }
+    return true;
+}
+
+void childHoldAction(void) {
+    // COMBACK: LOG
+    raise(SIGSTOP);
+    hold = false;
+}
+
+void childResumeAction(void) {
+    // COMBACK: LOG
+    resume = false;
+}
+
+void childTerminateAction(void) {
+    // COMBACK: LOG
+    terminate = false;
+}
+
+bool checkChildAction(void) {
+    if (hold) childHoldAction();
+    if (resume) childResumeAction();
+    if (terminate) {
+        childTerminateAction();
+        return true;
+    }
+    return false;
+}
+
+bool checkTerminateSignal(void) {
+    return isParentProcess() ? checkParentAction() : checkChildAction();
 }
