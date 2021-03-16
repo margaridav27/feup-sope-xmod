1af44ec2f21cf01c15b6fc41e97eb5db40d678c5
Tomás Vicente <up201904609@fe.up.pt>
2021-03-16 15:00:59 +0000

    Flags for signal handling.

--- project/src/signals.c
+++ project/src/signals.c
@@ -1,1 +1,11 @@
 #include "../include/signals.h"
+#include <signal.h>
+#include <stdbool.h>
+
+// Parent flags
+static volatile sig_atomic_t prompt = false; // Whether the user should be prompted
+
+// Child flags
+static volatile sig_atomic_t hold = false; // Whether the process should hold
+static volatile sig_atomic_t resume = false; // Whether the process should resume
+static volatile sig_atomic_t terminate = false; // Whether the process should terminate
